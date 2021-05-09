4b21d741f6b1add8c38f7f6c36b6127c1be860b3
21grahams <graham.kirsh@gmail.com>
2021-05-08 22:47:56 -0600

    frontend rendering to page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log('making it here?')
     return (
       <div>
         Hello Graham
--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const cors = require('cors');
 
+//=====================
+//     Middleware
+//=====================
+app.use(express.json()); // => req.body
+app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use(cors());
+
+//=====================
+//     Routes
+//=====================
 app.get('/', (req, res) => {
   res.send('Hello World YEEE!')
 })
