de844f14ab0c79cccaf31221a101aebe4c164ee6
21grahams <graham.kirsh@gmail.com>
2021-05-11 22:40:54 -0600

    cleaned up code and removed pseudocode

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-// import Sentence from './Sentence.jsx'
 
 const Map = ({data}) => {
-  // const items = data.description;
-  let item = data[Math.floor(Math.random() * data.length)];
+  const item = data[Math.floor(Math.random() * data.length)];
   console.log('item: ', item)
   return (
     <div>
