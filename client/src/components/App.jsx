61d7b8390933c5100054b1ab3fb82b50eef6a54a
21grahams <graham.kirsh@gmail.com>
2021-05-17 21:34:12 -0600

    cleaned up code in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,64 +1,72 @@
-import React from 'react';
-import Map from './Map.jsx'
-import axios from 'axios';
-import ModalHome from './ModalHome.jsx'
+import React from "react";
+import Map from "./Map.jsx";
+import axios from "axios";
+import ModalHome from "./ModalHome.jsx";
 import { Button, Modal } from "react-bootstrap";
 import { Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       data: [],
-      showModalState: false
-    }
+      showModalState: false,
+    };
     this.renderButton = this.renderButton.bind(this);
     this.addPost = this.addPost.bind(this);
     this.showModal = this.showModal.bind(this);
   }
 
   renderButton() {
-    axios.get('/dad')
-    .then(res => {
-      this.setState({
-        data: res.data
+    axios
+      .get("/dad")
+      .then((res) => {
+        this.setState({
+          data: res.data,
+        });
       })
-    })
-    .catch(err => console.log('ERROR WITH DATA FETCH: ', err))
+      .catch((err) => console.log("ERROR WITH DATA FETCH: ", err));
   }
 
   addPost(saying) {
-    axios.post('/dad', saying)
-    .then(res => console.log('Successful Post! ', res))
-    .catch(err => console.log('Error with post: ', err))
+    axios
+      .post("/dad", saying)
+      .then((res) => console.log("Successful Post! ", res))
+      .catch((err) => console.log("Error with post: ", err));
   }
 
   showModal() {
     this.setState({
-      showModalState: !this.state.showModalState
-    })
+      showModalState: !this.state.showModalState,
+    });
   }
 
   render() {
     return (
       <div>
-        <Button className='GetFed' onClick={this.renderButton}>Get Fed</Button>
-        <Map data={this.state.data}/>
+        <Button className="GetFed" onClick={this.renderButton}>
+          Get Fed
+        </Button>
 
-        <Button className='addNewSlang' variant="primary" onClick={this.showModal}>
-        Add New Slang
-      </Button>
+        <Map data={this.state.data} />
 
-      <ModalHome
-        addNewPost={this.addPost}
-        show={this.state.showModalState}
-        onHide={this.showModal}
-      />
+        <Button
+          className="addNewSlang"
+          variant="primary"
+          onClick={this.showModal}
+        >
+          Add New Slang
+        </Button>
 
+        <ModalHome
+          addNewPost={this.addPost}
+          show={this.state.showModalState}
+          onHide={this.showModal}
+        />
       </div>
-    )
+    );
   }
 }
 
