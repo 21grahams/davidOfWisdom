7cbfbab278da0f1e8201fcd92af3607a92711510
21grahams <graham.kirsh@gmail.com>
2021-05-22 11:08:21 -0600

    added button to conditionally rendered addDelete function

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Map = ({data}) => {
+
   const item = data[Math.floor(Math.random() * data.length)];
+
+  const addDelete = () => {
+    console.log('yuhhhhh')
+    return <button>Delete</button>
+  }
+
   return (
     <div>
-      {item ? <span style={{padding: '10px'}} className='RandomPhrase'>{item.description}</span> : null}
+      {item ? <span style={{padding: '10px'}} className='RandomPhrase'>{item.description}{addDelete()}</span> : null}
     </div>
   )
 }
