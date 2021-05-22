ee19ffed37e1707351ed8514b59fb520def9cb53
21grahams <graham.kirsh@gmail.com>
2021-05-22 11:28:09 -0600

    delete button added with border

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-const Map = ({data}) => {
-
+const Map = ({ data }) => {
   const item = data[Math.floor(Math.random() * data.length)];
 
   const addDelete = () => {
-    console.log('yuhhhhh')
-    return <button>Delete</button>
-  }
+    return (
+      <Button variant="secondary" className="DeleteButton">
+        Delete
+      </Button>
+    );
+  };
 
   return (
     <div>
-      {item ? <span style={{padding: '10px'}} className='RandomPhrase'>{item.description}{addDelete()}</span> : null}
+      {item ? (
+        <span style={{ padding: "10px" }} className="RandomPhrase">
+          {item.description}
+          {addDelete()}
+        </span>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 export default Map;
