951d43933b79d221168d03ef22583f1e9be4550d
21grahams <graham.kirsh@gmail.com>
2021-05-22 10:35:01 -0600

    fixed css render issue with padding

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 const Map = ({data}) => {
   const item = data[Math.floor(Math.random() * data.length)];
   return (
-    <span  style={{padding: '10px'}} className='RandomPhrase'>
-      {item ? item.description : null}
-    </span>
+    <div>
+      {item ? <span style={{padding: '10px'}} className='RandomPhrase'>{item.description}</span> : null}
+    </div>
   )
-
 }
 
 export default Map;
